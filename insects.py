7639c55f992ce60d138a47a4d78004b5a2a70dbb
OlegKatrinets <oleg.katrinets@gmail.com>
2021-06-10 01:54:16 +0300

    insects.py added

---
+++ insects.py
@@ -0,0 +1,10 @@
+class Insects:
+    def __init__(self, name, canFly):
+        self.name = name
+        self.canFly = canFly
+
+    def jump():
+        print('I can Jump')
+
+    def walk():
+        print('I can Walk')
